ee990c352f74d66cb3152ce3bd928c6e30c623ca
Jacute <73467353+Jacute@users.noreply.github.com>
2021-03-28 16:14:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import traceback
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.common.exceptions import TimeoutException
-import urlextract
 from selenium.webdriver.common.keys import Keys
 import re
 import time
@@ -16,38 +15,35 @@ try:
     options.set_preference("general.useragent.override", "user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0")
 
     options.set_preference("dom.webdriver.enabled", False)
-    # options.headless = True
+    options.headless = True
 
     driver = webdriver.Firefox(
         executable_path=GeckoDriverManager().install(),
         options=options
     )
-    driver.set_page_load_timeout(10)
 except Exception as e:
     print('Неудачная настройка браузера!')
     print(traceback.format_exc())
     print(input('Press ENTER to close this program'))
     sys.exit()
-extractor = urlextract.URLExtract()
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 urls = []
 data_without_mail = []
 data = []
-regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-regex_url1 = 'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
-regex_url2 = 'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+regex = r"([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)"
+regex_url1 = '(?P<url>https?://[^\s]+)'
+regex_url2 = '(?P<url>http?://[^\s]+)'
 actions = webdriver.ActionChains(driver)
 
 try:
     with open('data.txt', mode='r') as f:
         file = f.read().split('\n')
+    with open('exceptions.txt', mode='r') as f:
+        exceptions = f.read().split('\n')
     for i in file:
         if i != '':
             print(f'Парсинг данных со всех запросов из data.txt: {file.index(i) + 1} из {len(file)}')
-            try:
-                driver.get(f'https://www.youtube.com/results?search_query={i}')
-            except TimeoutException:
-                continue
+            driver.get(f'https://www.youtube.com/results?search_query={i}')
             driver.implicitly_wait(5)
             while True:
                 scroll_height = 2000
@@ -62,8 +58,11 @@ try:
     description_urls = []
     for i in range(len(urls)):
         print(f'Парсинг ссылок из описания {i + 1} из {len(urls)}')
-        driver.get(urls[i])
-        driver.implicitly_wait(5)
+        try:
+            driver.get(urls[i])
+            driver.implicitly_wait(15)
+        except Exception:
+            continue
         try:
             more = driver.find_element_by_xpath('//paper-button[@id="more"]')
             more.send_keys(Keys.PAGE_DOWN)
@@ -71,41 +70,36 @@ try:
             more.click()
         except Exception:
             pass
-        description = driver.find_element_by_xpath('//div[@id="description"]').text
+        try:
+            description = driver.find_element_by_xpath('//div[@id="description"]').text
+        except Exception:
+            continue
         urls1 = re.findall(regex_url1, description)
         urls2 = re.findall(regex_url2, description)
-        for j in urls1:
-            if 'youtu' not in j and 'https://vk' not in j and 'instagram' not in j and 'https://ok' not in j and\
-                    'http://vk' not in j and 'facebook' not in j:
+        k = urls1 + urls2
+        for j in k:
+            for q in exceptions:
+                if q in j:
+                    break
+            if q not in j:
                 description_urls.append(j)
-        for j in urls2:
-            if 'youtu' not in j and 'https://vk' not in j and 'instagram' not in j and 'https://ok' not in j and \
-                    'http://vk' not in j and 'facebook' not in j:
-                description_urls.append(j)
+    driver.set_page_load_timeout(10)
     for q in range(len(description_urls)):
         print(f'Пытаемся достать почты из сайтов {q + 1} из {len(description_urls)}')
         email = ''
         url = description_urls[q]
         try:
             driver.get(url)
-            driver.implicitly_wait(10)
+            driver.implicitly_wait(15)
         except Exception:
             continue
         try:
-            body = driver.find_element_by_xpath('/html').text.split()
-            for i in range(len(body)):
-                try:
-                    if '@' in body[i]:
-                        if (re.search(regex, body[i])):
-                            email = body[i]
-                            break
-                        else:
-                            email = ''
-                except Exception:
-                    email = ''
+            body = driver.find_element_by_xpath('/html').text
+            email = re.findall(regex, body)[-1]
         except Exception:
-            pass
+            email = ''
         data.append((url, email))
+        driver.implicitly_wait(15)
 
     with open('result.csv', mode='w', newline='') as f:
         print('Запись в csv...')
